56f13ccac877ac97d9f2c181285c8efee90c5bbf
Victor Grigoriu <vgrigoriu@gmail.com>
2014-11-24 10:25:28 +0200

    Create tests for ElementData

---
+++ src/Pig/RenderingEngine.Tests/ElementDataTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Immutable;
+using Xunit;
+
+namespace RenderingEngine.Tests
+{
+    public class ElementDataTests
+    {
+        [Fact]
+        public void TagNameIsSetViaConstructorParameter()
+        {
+            var tagName = "blink";
+            var noAttributes = ImmutableDictionary.Create<string, string>();
+
+            var sut = new ElementData(tagName, noAttributes);
+
+            Assert.Equal(tagName, sut.TagName);
+        }
+
+        [Fact]
+        public void AttributesAreSetViaConstructorParameter()
+        {
+            var tagName = "blink";
+            var noAttributes = ImmutableDictionary.Create<string, string>();
+
+            var sut = new ElementData(tagName, noAttributes);
+
+            Assert.Equal(noAttributes, sut.Attributes);
+        }
+    }
+}
