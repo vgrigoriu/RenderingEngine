86773e50e2f1ef9c1400d12fcc11a6aaf5604492
Victor Grigoriu <vgrigoriu@gmail.com>
2014-11-24 09:56:04 +0200

    Create class ElementData

---
+++ src/Pig/RenderingEngine/ElementData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+
+namespace RenderingEngine
+{
+    public class ElementData
+    {
+        public string TagName { get; }
+
+        public ImmutableDictionary<string, string> Attributes { get; }
+
+        public ElementData(string tagName, ImmutableDictionary<string, string> attributes)
+        {
+            TagName = tagName;
+            Attributes = attributes;
+        }
+    }
+}
